11815ad7ba2fd9a532dd8749b97817e2a289bd2b
yloose <47208829+yloose@users.noreply.github.com>
2022-10-11 14:02:29 +0200

    Update version-bump.sh

--- version-bump.sh
+++ version-bump.sh
@@ -54,7 +54,7 @@ else
   bump $BUMP_MODE $OLD_VERSION
   echo "pom.xml at" $POMPATH "will be bumped from" $OLD_VERSION "to" $NEW_VERSION
   mvn -q versions:set -DnewVersion="${NEW_VERSION}" -DprocessAllModules
-  git add $POMPATH/pom.xml
+  git add $POMPATH/**/pom.xml
   REPO="https://$GITHUB_ACTOR:$TOKEN@github.com/$GITHUB_REPOSITORY.git"
   git commit -m "Bump pom.xml from $OLD_VERSION to $NEW_VERSION"
   git tag $NEW_VERSION
